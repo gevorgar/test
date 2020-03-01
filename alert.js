aacd6c2b28f72fca00c228db99c08d15a3922424
Gevorg <gevorg.ar.23@gmail.com>
2020-03-01 20:45:55 +0200

    first comit

---
+++ alert.js
@@ -0,0 +1,24 @@
+'use strict'
+
+a = 5;
+console.log(a)
+// let arr = ['plum.png','orange.jpg','apple.bmp']
+// console.log(arr[0])
+
+// let answer =+prompt("есть ли вам 18", "Да")
+// console.log(typeof(answer))
+
+let incr = 10, 
+decr = 10;
+
+
+console.log(incr++);
+console.log(decr--)
+console.log(5%2);
+console.log("2"==2);
+
+let isCheked = false,
+    isClose = false;
+
+console.log(isCheked || isClose);
+
